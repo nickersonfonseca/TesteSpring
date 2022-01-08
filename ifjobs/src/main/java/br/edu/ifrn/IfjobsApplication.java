0e056524853cf7d385e3241dfbca1d02b4276c11
Nickerson <Nickerson@10.98.3.43>
2022-01-08 09:34:53 -0300

    Commit inicial de projeto

---
+++ ifjobs/src/main/java/br/edu/ifrn/IfjobsApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfjobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfjobsApplication.class, args);
+	}
+
+}
---
+++ ifjobs/src/test/java/br/edu/ifrn/IfjobsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfjobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
